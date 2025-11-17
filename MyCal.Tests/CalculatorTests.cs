738c5c715a3d987257774b829a9ef37ca81f5afa
kihun.ahn <khahn0721@gmail.com>
2025-11-17 21:35:31 +0900

    Support Sub

--- MyCal.Tests/CalculatorTests.cs
+++ MyCal.Tests/CalculatorTests.cs
@@ -9,5 +9,12 @@ namespace MyCal.Tests
             var cal = new Calculator();
             Assert.True(8 == cal.Add(3, 5));
         }
+
+        [Fact]
+        public void SubTest()
+        {
+            var cal = new Calculator();
+            Assert.True(8 == cal.Sub(3, 5));
+        }
     }
 }
--- MyCal/Calculator.cs
+++ MyCal/Calculator.cs
@@ -3,5 +3,7 @@
     public class Calculator
     {
         public double Add(double a, double b) => a + b;
+
+        public double Sub(double a, double b) => a - b;
     }
 }
